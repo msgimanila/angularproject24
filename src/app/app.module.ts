fbf4eef895a88b36e3850ebbf3388af319c77328
Mario Garcia <msgimanila@yahoo.com>
2024-12-08 19:47:48 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import NewsFeed from './components/NewsFeed';
+import NewsFeed from '.NewsFeed';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from 'app-routing.module';
+import { AppComponent } from 'app.component';
 
 @NgModule({
   declarations: [
