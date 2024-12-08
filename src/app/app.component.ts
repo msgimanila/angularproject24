f59a358d675190e17cc209766ec638671fd049a4
Mario Garcia <msgimanila@yahoo.com>
2024-12-08 19:40:28 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewsFeedComponent } from './components/news-feed/news-feed.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewsFeedComponent // Declare NewsFeedComponent here
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppComponent {
-  title = 'angular-quickstart';
-}
+export class AppModule {}
